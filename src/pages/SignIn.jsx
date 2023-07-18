4aaf66559dc40d07ccf550f9037635ff02b0bc8e
patrick mugambi <patrick@kali.patrick>
2023-07-18 15:35:18 +0300

    create sign in, sign up & forgot password pages

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { Link } from 'react-router-dom';
+import OAuth from '../components/OAuth';
 const SignIn = () => {
 const [showPassword, setShowPassword] = useState(false);
 const [formData, setFormData] = useState({
@@ -29,13 +31,13 @@ function onChange(e) {
           <form>
             <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white
             border-gray-300 rounded
-            transition ease-in-out'
+            transition ease-in-out mb-6'
              type='email' id='email'
             value={email} 
             onChange={onChange}
             placeholder='Email address'
         ></input>
-        <div>
+        <div className='relative mb-6'>
         <input className='w-full px-4 py-2 text-xl text-gray-700 bg-white
             border-gray-300 rounded
             transition ease-in-out'
@@ -46,11 +48,44 @@ function onChange(e) {
             placeholder='Password'
         ></input>
         {showPassword ? 
-            <AiFillEyeInvisible/> : 
-            AiFillEye 
+            <AiFillEyeInvisible 
+            className='absolute right-3 top-3 text-xl cursor-pointer'
+            onClick={() => setShowPassword
+              ((prevState) => !prevState)
+            }
+            
+           /> : 
+            <AiFillEye
+            className='absolute right-3 top-3 text-xl cursor-pointer'
+            onClick={() => setShowPassword
+              ((prevState) => !prevState)
+            }
+            />
             }
         </div>
+        <div className='flex justify-between
+        whitespace-nowrap text-sm sm:text-lg
+        '>
+          <p className='mb-6 '>Don't have a account?
+            <Link to="/sign-up" className='text-red-600
+            hover:red-700 transition duration-200 ease-in-out ml-1'>Register</Link>
+          </p>
+          <p>
+            <Link to="/forgot-password" className='   hover:blue-700 transition duration-200 ease-in-out  text-blue-600' >Forgot password?</Link>
+          </p>
+        </div>
+        <button
+              className="w-full bg-blue-600 text-white px-7 py-3 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-150 ease-in-out hover:shadow-lg active:bg-blue-800"
+              type="submit"
+            >
+              Sign in
+            </button>
+            <div className="flex items-center  my-4 before:border-t before:flex-1 before:border-gray-300 after:border-t after:flex-1 after:border-gray-300">
+              <p className="text-center font-semibold mx-4">OR</p>
+            </div>
+            <OAuth />
           </form>
+     
         </div>
       </div>
     </section>
